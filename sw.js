591313f38317ca284ba86609a4bf8e26dc101e34
abutamse <abodymar.2012@gmail.com>
2026-02-06 18:18:54 +0100

    Implement service worker event listeners
    Add event listeners for install, activate, and fetch events in the service worker.

--- sw.js
+++ sw.js
@@ -1,3 +1,11 @@
-if ("serviceWorker" in navigator) {
-  navigator.serviceWorker.register("sw.js");
-}
+self.addEventListener('install', event => {
+  console.log('Service Worker installiert');
+});
+
+self.addEventListener('activate', event => {
+  console.log('Service Worker aktiviert');
+});
+
+self.addEventListener('fetch', event => {
+  // Einfach alles normal laden, wir brauchen hier keinen Cache
+});
